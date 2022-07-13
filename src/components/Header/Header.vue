510a1d9c3794aad8bd66c895925918568c5f60b3
AlexandrM96 <super.dwtd2013@yandex.ru>
2022-07-13 23:02:30 +0300

    test

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,50 @@
+<template>
+    <header class="header">
+        <div class="header__container">
+            <div class="header__container__logo-and-buttons">
+                <div class="container-logo">
+                    <div class="container-logo__image">
+                        <img alt="Vue logo" src="../../assets/image/logo.svg" />
+                    </div>
+                </div>
+                <div class="container-buttons">
+                    <nav>
+                        <li>Product</li>
+                        <li>Integrations</li>
+                        <li>How it works</li>
+                    </nav>
+                    <div>
+                        <button>Contact form</button>
+                        <button>Sign up</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#header {
+    box-shadow: 0px 0px 2px rgba(145, 158, 171, 0.24)
+}
+
+#header__container {
+    width: 1440px;
+    padding: 20px;
+}
+
+#header__container__logo-and-buttons {
+    display: flex;
+    justify-content: space-between;
+}
+</style>
---
+++ src/components/Main/Main.vue
@@ -0,0 +1,35 @@
+<template>
+  <main class="main">
+   <Header />
+   this is main!!!
+  </main>
+</template>
+
+<script>
+import Header from '../Header/Header'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
